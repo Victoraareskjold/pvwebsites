6d7888030ef0f2c79739964443219f754dcda8da
Victor Aareskjold <victor.aaareskjold@gmail.com>
2025-01-26 12:46:05 +0100

    ednringer4 fixes

--- src/app/solkart/layout.js
+++ src/app/solkart/layout.js
@@ -1,12 +1,28 @@
 import { Navbar } from "@/components/Navbar";
+import { SiteConfigProvider } from "@/contexts/siteConfigContext";
+import { headers } from "next/headers";
 import "../globals.css";
 
-export default function SolkartLayout({ children }) {
+const configs = {
+  vestelektro: () => import("@/config/vestelektro"),
+  alfaelektro: () => import("@/config/alfaelektro"),
+  lynelektro: () => import("@/config/lynelektro"),
+};
+
+export default async function SolkartLayout({ children }) {
+  const headersList = await headers();
+  const configName = headersList.get("x-site-config");
+
+  const configModule = configName ? await configs[configName]() : {};
+  const config = configModule.default || {};
+
+  config.language = configName === "vestelektro" ? "nn" : "nb";
+
   return (
     <html>
       <body>
-        <Navbar />
-        {children}
+        <Navbar logo={config.logo} title={config.title} />
+        <SiteConfigProvider config={config}>{children}</SiteConfigProvider>
       </body>
     </html>
   );
--- src/app/solkart/page.js
+++ src/app/solkart/page.js
@@ -8,7 +8,7 @@ export default function Solkart() {
   return (
     <div style={{ backgroundColor: "#1E1E1E" }} className="h-screen">
       <iframe
-        src={`https://pvmap.vercel.app/?site=null}`}
+        src={`https://pvmap.vercel.app/?site=${config.title}}`}
         className="h-full lg:!pb-0"
         width="100%"
         style={{ paddingTop: "72px" }}
