ca881d3151ea15505b1b4216296476a2ff63c251
Victor Aareskjold <victor.aaareskjold@gmail.com>
2025-02-01 13:14:11 +0100

    potential analytics

--- src/app/solkart/layout.js
+++ src/app/solkart/layout.js
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/Navbar";
 import { SiteConfigProvider } from "@/contexts/siteConfigContext";
 import { headers } from "next/headers";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "../globals.css";
 
 const configs = {
@@ -20,6 +21,51 @@ export default async function SolkartLayout({ children }) {
 
   return (
     <html className="h-full">
+      <head>
+        {config.googleTagManager && (
+          <GoogleTagManager gtmId={config.googleTagManager} />
+        )}
+
+        {config.hubspotScript && (
+          <script
+            type="text/javascript"
+            id="hs-script-loader"
+            async
+            defer
+            src={config.hubspotScript}
+          ></script>
+        )}
+
+        {config.metaPixel && (
+          <>
+            <script
+              id="meta-pixel"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '${config.metaPixel}');
+                  fbq('track', 'PageView');`,
+              }}
+            />
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src={`https://www.facebook.com/tr?id=${config.metaPixel}&ev=PageView&noscript=1`}
+              />
+            </noscript>
+          </>
+        )}
+      </head>
       <body className="h-full">
         <Navbar logo={config.logo} title={config.title} pos={"absolute"} />
         <SiteConfigProvider config={config}>{children}</SiteConfigProvider>
