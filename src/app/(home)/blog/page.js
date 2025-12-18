d7271bdb82cd0b73b6fb85cf5a136b898155676e
Victor Aareskjold <victor.aareskjold@icloud.com>
2025-12-18 13:34:28 +0100

    first

--- src/app/(home)/blog/page.js
+++ src/app/(home)/blog/page.js
@@ -1,4 +1,5 @@
 "use client";
+
 import { useSiteConfig } from "../../../contexts/siteConfigContext";
 import blogs from "../../../config/blogs.json";
 import "./blog.css";
@@ -9,7 +10,7 @@ import Image from "next/image";
 export default function Blog() {
   const config = useSiteConfig();
 
-  console.log(config.name);
+  if (!config) return <p>Laster...</p>;
 
   const filteredBlogs = blogs.filter((blog) => {
     if (config.name !== "alfaelektro" && blog.id === 12) {
@@ -18,7 +19,7 @@ export default function Blog() {
     return true;
   });
 
-  const sortedBlogs = filteredBlogs.sort((a, b) => b.id - a.id);
+  const sortedBlogs = [...filteredBlogs].sort((a, b) => b.id - a.id);
 
   return (
     <section className="py-24 bg-black p-4 min-h-screen">
@@ -26,20 +27,23 @@ export default function Blog() {
         {config.blogPage?.header ||
           "Ta en titt på det nyeste vi har skrevet, og bla deg bakover i tid."}
       </h1>
-      <ul className="grid gap-4 xl:gap-8 md:grid-cols-2 xl:grid-cols-3 ">
+
+      <ul className="grid gap-4 xl:gap-8 md:grid-cols-2 xl:grid-cols-3">
         {sortedBlogs.map((blog) => (
           <Link
             key={blog.id}
-            className="relative w-full h-64 rounded-2xl overflow-hidden"
             href={`/blog/${blog.slug}`}
+            className="relative w-full h-64 rounded-2xl overflow-hidden"
           >
             <Image
               fill
               src={blog.image}
-              alt={blog.title || "Blogg bilde"}
-              className="object-cover text-white"
+              alt={blog.nb.title}
+              className="object-cover"
             />
-            <h2 className="mt-2 text-white font-bold">{blog.nb.title}</h2>
+            <h2 className="absolute bottom-4 left-4 right-4 text-white font-bold">
+              {blog.nb.title}
+            </h2>
           </Link>
         ))}
       </ul>
