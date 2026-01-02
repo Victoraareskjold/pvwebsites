1ddaeb4d38d6fcb3ccea6da5b07df179cd1c2945
Victor Aareskjold <victor.aareskjold@icloud.com>
2026-01-02 12:31:53 +0100

    styling buttons

--- src/components/EstimateButton.js
+++ src/components/EstimateButton.js
@@ -4,12 +4,15 @@ export function EstimateButton({ setModalOpen }) {
   return (
     <button
       onClick={setModalOpen}
-      style={{ background: config.primaryGradient?.bg || "black" }}
-      className="p-2 rounded-md text-white md:border-2 border-black md:max-w-96 md:w-full text-center md:hover:!bg-white md:hover:!text-black duration-500"
+      style={{
+        background:
+          config.primaryGradient?.bg ||
+          "linear-gradient(90deg, #FF9D00 23%, #FFD05A 92%)",
+      }}
+      //
+      className="p-2 rounded-md text-white md:border-2 border-white md:max-w-96 md:w-full text-center md:hover:!bg-white md:hover:!text-black duration-500"
     >
-      <h2 className="md:!text-black">
-        {config.estimateBtn?.text || "Jeg ønsker tilbud"}
-      </h2>
+      <h2>{config.estimateBtn?.text || "Jeg ønsker tilbud"}</h2>
     </button>
   );
 }
--- src/components/EstimateButton2.js
+++ src/components/EstimateButton2.js
@@ -7,12 +7,9 @@ export function EstimateButton2({ setModalOpen }) {
       style={{
         background: "linear-gradient(90deg, #FF9D00 23%, #FFD05A 92%)",
       }}
-      className="solcelleknapp max-w-sm funky p-2 rounded-md text-black flex flex-row gap-2 md:max-w-96 md:w-full justify-center hover:!bg-black hover:!border-white hover:!border-2 "
+      className="solcelleknapp max-w-sm funky p-2 rounded-md text-white flex flex-row gap-2 md:max-w-96 md:w-full justify-center hover:!bg-black hover:!border-white border-2 duration-1000"
     >
-      <h2
-        style={{ color: config.primaryGradient?.textColor || "white" }}
-        className="md:!text-white"
-      >
+      <h2 style={{ color: "white" }}>
         {config.estimateBtn?.text || "Jeg ønsker tilbud"}
       </h2>
     </button>
