11d487f118da7dc743998dd0442760bb62304fc7
Victor Aareskjold <victor.aareskjold@icloud.com>
2026-01-13 10:18:00 +0100

    fix

--- src/config/mydlands.js
+++ src/config/mydlands.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  title: "Mydlandss Elektriske Sol",
+  title: "Mydlands Elektriske Sol",
 
   site: "Mydlandsselektriskesol",
 
