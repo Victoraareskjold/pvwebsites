93ab6480b5b4049812dd8ed93d0e9c22755c2c66
Victor Aareskjold <victor.aareskjold@icloud.com>
2026-01-24 16:24:54 +0100

    fix: js not ts

--- utils/localstorage.js
+++ utils/localstorage.js
@@ -1,11 +1,11 @@
-export const setLocalStorage = (key: string, value: string) => {
+export const setLocalStorage = (key, value) => {
   if (typeof window === "undefined") return;
   try {
     localStorage.setItem(key, value);
   } catch {}
 };
 
-export const getLocalStorage = (key: string): string | null => {
+export const getLocalStorage = (key) => {
   if (typeof window === "undefined") return null;
   try {
     return localStorage.getItem(key);
