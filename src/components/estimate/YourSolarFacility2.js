905f681fba2e79bc8ca6906c40c606e139e89db0
Victor Aareskjold <victor.aareskjold@icloud.com>
2026-02-02 12:07:06 +0100

    fix: displaying correct information in solarfacility

--- src/components/estimate/YourSolarFacility2.js
+++ src/components/estimate/YourSolarFacility2.js
@@ -34,57 +34,57 @@ export default function YourSolarFacility2({ estimateData, finished }) {
           type="INSTALLERT EFFEKT (KWP)"
           attachmentUrl={estimateData?.simulation_pdf}
         />
+        <div className="w-full h-2 bg-green-300 rounded-full my-6" />
         {panelItems.map((item, index) => (
           <div key={item.id}>
-            <div className="w-full h-2 bg-green-300 rounded-full my-6" />
             <EstimateInfoComponent
               text={`${item.product} panel`}
               number={`${item.quantity} stk -`}
               image={"/estimate/info2.png"}
               finished={finished}
-              type="SOLCELLEPANEL"
+              type={index === 0 ? "SOLCELLEPANEL" : ""}
               attachmentUrl={item.attachmentUrl}
             />
           </div>
         ))}
 
+        <div className="w-full h-2 bg-green-300 rounded-full my-6" />
         {inverterItems.map((item, index) => (
           <div key={item.id}>
-            <div className="w-full h-2 bg-green-300 rounded-full my-6" />
             <EstimateInfoComponent
               text={`${item.product} inverter`}
               number={`${item.quantity} stk`}
               image={"/estimate/info3.png"}
               finished={finished}
-              type="INVERTER"
+              type={index === 0 ? "INVERTER" : ""}
               attachmentUrl={item.attachmentUrl}
             />
           </div>
         ))}
 
+        <div className="w-full h-2 bg-green-300 rounded-full my-6" />
         {mountingItems.map((item, index) => (
           <div key={item.id}>
-            <div className="w-full h-2 bg-green-300 rounded-full my-6" />
             <EstimateInfoComponent
               text={`${item.product ?? "Ingen valgt"} feste`}
               number={`${item.quantity ?? 0} stk`}
               image={"/estimate/info4.png"}
               finished={finished}
-              type="FESTESYSTEM"
+              type={index === 0 ? "FESTESYSTEM" : ""}
               attachmentUrl={item.attachmentUrl}
             />
           </div>
         ))}
 
+        <div className="w-full h-2 bg-green-300 rounded-full my-6" />
         {batteryItems.map((item, index) => (
           <div key={item.id}>
-            <div className="w-full h-2 bg-green-300 rounded-full my-6" />
             <EstimateInfoComponent
               text={`${item.product} batteri`}
               number={`${item.quantity} stk -`}
               image={"/estimate/info2.png"}
               finished={finished}
-              type="BATTERI"
+              type={index === 0 ? "BATTERI" : ""}
               attachmentUrl={item.attachmentUrl}
             />
           </div>
