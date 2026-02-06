a6ea00f12076299ed9600f2b89bce120f79a2abd
Victor Aareskjold <victor.aareskjold@icloud.com>
2026-02-06 15:55:30 +0100

    fix: solkart client/server

---
+++ src/app/[site]/solkart/SolkartClient.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { getLocalStorage } from "../../../../utils/localstorage";
+
+export default function SolkartClient({ site }) {
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const gclid = getLocalStorage("gclid") ?? "";
+    const fbclid = getLocalStorage("fbclid") ?? "";
+    const utm = getLocalStorage("utmCampaign") ?? "";
+
+    const params = new URLSearchParams({
+      site,
+      ...(gclid && { gclid }),
+      ...(fbclid && { fbclid }),
+      ...(utm && { utm_campaign: utm }),
+    });
+
+    setQuery(params.toString());
+  }, [site]);
+
+  return (
+    <div style={{ backgroundColor: "#1E1E1E" }} className="h-full">
+      <iframe
+        src={`https://pvmap.vercel.app/?${query}`}
+        className="h-full lg:!pb-0"
+        width="100%"
+        style={{ paddingTop: "86px" }}
+      />
+    </div>
+  );
+}
--- src/app/[site]/solkart/page.js
+++ src/app/[site]/solkart/page.js
@@ -1,15 +1,7 @@
+import SolkartClient from "./SolkartClient";
+
 export default async function Solkart({ params }) {
   const { site } = await params;
-  console.log(site);
 
-  return (
-    <div style={{ backgroundColor: "#1E1E1E" }} className="h-full">
-      <iframe
-        src={`https://pvmap.vercel.app/?site=${site}`}
-        className="h-full lg:!pb-0"
-        width="100%"
-        style={{ paddingTop: "86px" }}
-      />
-    </div>
-  );
+  return <SolkartClient site={site} />;
 }
